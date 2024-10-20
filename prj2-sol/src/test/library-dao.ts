e408f4b00c1df75a2b3cd262ac9ecdd31d3f3b0b
Nick DeNobrega <nickdenobrega@gmail.com>
2024-10-20 16:33:44 -0400

    Added tests

--- prj2-sol/src/test/library-dao.ts
+++ prj2-sol/src/test/library-dao.ts
@@ -29,6 +29,54 @@ describe('library DAO', () => {
 
   //TODO: add test suites here as needed to test your DAO as you implement it
   //(your DAO is available as variable "dao").
+  it('should checkout a book for a patron', async () => {
+    const patronId = PATRONS[0];
+    const isbn = ISBNS[0];  
+    await dao.checkoutBook(patronId, isbn);
+    // Check if the book is checked out to the patron
+    const hasBook = await dao.checkPatronHasBook(isbn, patronId);
+    expect(hasBook).to.be.true;
+    // Check if the number of checkouts for the book increased
+    const numCheckouts = await dao.getNumBookCheckouts(isbn);
+    expect(numCheckouts).to.equal(1);
+  });
+
+  it('should return a book for a patron', async () => {
+    const patronId = PATRONS[1];
+    const isbn = ISBNS[1];  
+    // First, checkout a book
+    await dao.checkoutBook(patronId, isbn);
+    // Then, return the book
+    await dao.returnBook(patronId, isbn);
+    // Verify the book is no longer checked out
+    const hasBook = await dao.checkPatronHasBook(isbn, patronId);
+    expect(hasBook).to.be.false;
+    // Ensure the number of checkouts for the book is zero
+    const numCheckouts = await dao.getNumBookCheckouts(isbn);
+    expect(numCheckouts).to.equal(0);
+  });
+
+  it('should get the number of checkouts for a book', async () => {
+    const isbn = ISBNS[2];
+    // Checkout the book for multiple patrons
+    await dao.checkoutBook(PATRONS[0], isbn);
+    await dao.checkoutBook(PATRONS[2], isbn);
+    // Verify the number of checkouts is correct
+    const numCheckouts = await dao.getNumBookCheckouts(isbn);
+    expect(numCheckouts).to.equal(2);
+  });
+
+  it('should check if a patron has a book checked out', async () => {
+    const patronId = PATRONS[2]; 
+    const isbn = ISBNS[0];
+    await dao.checkoutBook(patronId, isbn);
+    // Check if patron has the book checked out
+    const hasBook = await dao.checkPatronHasBook(isbn, patronId);
+    expect(hasBook).to.be.true;
+    // Check for a different patron who didn't checkout the book
+    const otherPatronHasBook = await dao.checkPatronHasBook(isbn, PATRONS[0]);
+    expect(otherPatronHasBook).to.be.false;
+  });
 
 });
 
